55855009475e9fdb806e3c1db560e963fb935d91
Michał Rubel <contact@observer.name>
2022-08-06 15:23:02 +0200

    improve tests

--- tests/PipelineEventsTest.php
+++ tests/PipelineEventsTest.php
@@ -4,15 +4,59 @@ namespace MichaelRubel\EnhancedPipeline\Tests;
 
 use Illuminate\Support\Facades\Event;
 use MichaelRubel\EnhancedPipeline\Events\PipePassed;
+use MichaelRubel\EnhancedPipeline\Events\PipeStarted;
 use MichaelRubel\EnhancedPipeline\Pipeline;
 
 class PipelineEventsTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Event::fake();
+    }
+
+    /** @test */
+    public function testFiresPipeStartedEvents()
+    {
+        app(Pipeline::class)
+            ->send('data')
+            ->through([
+                TestPipe::class,
+                TestPipe::class,
+            ])
+            ->thenReturn();
+
+        Event::assertDispatched(function (PipeStarted $event) {
+            $this->assertInstanceOf(TestPipe::class, app($event->pipe));
+            $this->assertSame('data', $event->passable);
+
+            return true;
+        }, 2);
+    }
+
+    /** @test */
+    public function testFiresPipeStartedEventsButFailsToPass()
+    {
+        app(Pipeline::class)
+            ->send('data')
+            ->through(PipelineWithException::class)
+            ->onFailure(fn () => true)
+            ->thenReturn();
+
+        Event::assertDispatched(function (PipeStarted $event) {
+            $this->assertInstanceOf(PipelineWithException::class, app($event->pipe));
+            $this->assertSame('data', $event->passable);
+
+            return true;
+        });
+
+        Event::assertNotDispatched(PipePassed::class);
+    }
+
     /** @test */
     public function testFiresPipePassedEvents()
     {
-        Event::fake();
-
         app(Pipeline::class)
             ->send('data')
             ->through([
