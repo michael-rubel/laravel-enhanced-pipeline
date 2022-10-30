33a6053be0c398a4a307e00545d9d8de5f2a6ff9
Michael Rubél <contact@observer.name>
2022-10-30 17:04:43 +0100

    Add tests for `run` helper 🧪

---
+++ tests/PipelineRunHelperTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MichaelRubel\EnhancedPipeline\Tests;
+
+use MichaelRubel\EnhancedPipeline\Pipeline;
+
+class PipelineRunHelperTest extends TestCase
+{
+    public function testRunHelperWithoutParams()
+    {
+        $executed = run(Action::class);
+
+        $this->assertTrue($executed);
+    }
+
+    public function testRunHelperActionReturnsPassedData()
+    {
+        $executed = run(Action::class, ['test' => 'yeah']);
+
+        $this->assertSame('yeah', $executed['test']);
+    }
+
+    public function testRunHelperHasCustomizableMethod()
+    {
+        $this->app->resolving(Pipeline::class, function ($pipeline) {
+            return $pipeline->via('execute');
+        });
+
+        $executed = run(ActionExecute::class);
+
+        $this->assertTrue($executed);
+    }
+}
+
+class Action
+{
+    public function handle(mixed $data, \Closure $next): mixed
+    {
+        return $next($data);
+    }
+}
+
+class ActionExecute
+{
+    public function execute(mixed $data, \Closure $next): mixed
+    {
+        return $next($data);
+    }
+}
