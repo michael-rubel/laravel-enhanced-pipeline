9ae7877121430741bd90c0bbc41df3ec905a9c4b
Michael Rubél <contact@observer.name>
2024-03-17 14:20:25 +0100

    Improve types for destination in events

--- src/Events/PipelineFinished.php
+++ src/Events/PipelineFinished.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace MichaelRubel\EnhancedPipeline\Events;
 
+use Closure;
+
 class PipelineFinished
 {
     public function __construct(
-        public mixed $destination,
+        public Closure $destination,
         public mixed $passable,
         public array $pipes,
         public bool $useTransaction,
