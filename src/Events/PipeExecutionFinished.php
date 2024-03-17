7ba7536906009f5a090f8d4fee37e7e70c32a5d5
Michael Rubél <contact@observer.name>
2024-03-17 14:15:40 +0100

    Add types to old events

--- src/Events/PipeExecutionFinished.php
+++ src/Events/PipeExecutionFinished.php
@@ -6,12 +6,10 @@ namespace MichaelRubel\EnhancedPipeline\Events;
 
 class PipeExecutionFinished
 {
-    /**
-     * @param  mixed  $pipe
-     * @param  mixed  $passable
-     */
-    public function __construct(public $pipe, public $passable)
-    {
+    public function __construct(
+        public mixed $pipe,
+        public mixed $passable,
+    ) {
         //
     }
 }
